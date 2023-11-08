8ade7fa56700019e31a5cd6d313df657f2dd981b
Zach Ezwawi <ezwawiz@gmail.com>
2023-11-08 02:28:50 -0500

    Refactor code. Add frequently used func to context

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
 'use client'
 
 import Container from '@/components/timer/container'
-import Experience from '@/components/timer/experience'
-import ResetButton from '@/components/timer/reset-button'
-import StartButton from '@/components/timer/start-button'
-import TimeButton from '@/components/timer/time-button'
-import Timer from '@/components/timer/timer'
 import { useTimerContext } from '@/context/timer-context'
-import { Canvas } from '@react-three/fiber'
-import Image from 'next/image'
 
 export default function Home() {
   const { seconds } = useTimerContext()
